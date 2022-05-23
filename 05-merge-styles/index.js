64aa13690b53fb7ddf80594512c48910d26a554a
Tatiana <amora4ka@gmail.com>
2022-05-23 23:11:23 +0300

    feat: do 5 task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs/promises');
+const bundle = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+async function findFiles(dirPath) {
+  const assets = await readdir(dirPath, { withFileTypes: true });
+  for (const asset of assets) {
+    if (asset.isFile() && path.extname(asset.name) === '.css') {
+      const cssFile = fs.createReadStream(path.join(dirPath, asset.name), 'utf-8');
+      cssFile.on('data', (chunk) => {
+        bundle.write(chunk);
+      });
+    }
+  }
+}
+
+findFiles(path.join(__dirname, 'styles'));
